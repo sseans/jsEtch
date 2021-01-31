34803c603f7d1b29cd717342bbe7bfc7dd772ce2
sseans <41116820+sseans@users.noreply.github.com>
2021-01-31 15:32:13 -0700

    Added drawing functionality and resizable grid

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
-const canvas = document.querySelector('.canvas')
+// Variables
 let canvasSize = 32;
-let canvasPart = null;
+let chosenColor = 'yellow'
 
+// Selectors
+const canvas = document.querySelector('.canvas')
+
+// Functions
 function createCanvasSegments() {
     for (let i = 0; i < canvasSize; i++) {
         for (let i = 0; i < canvasSize; i++) {
-            canvasPart = document.createElement('div')
+            const canvasPart = document.createElement('div')
             canvasPart.className = 'canvas__part'
             canvas.appendChild(canvasPart)
+
+            let cssrule = calculatePartSize(canvasPart, canvasSize)
+            addEventlistenerToPart(canvasPart, cssrule)
         }
-    }
-    
+    }  
 }
 
-function calculateCanvasPartSize() {
-    let widthHeight = 100 / canvasSize
-    console.log(widthHeight);
-    
-    let cssText = "`height:${widthHeight}%; width:${widthHeight}%;`"
-    canvasPart.style.cssText(cssText)
+function calculatePartSize(component, desiredSize) {
+    let widthHeight = 100 / desiredSize   
+    component.style.cssText=`height: ${widthHeight}%; width: ${widthHeight}%;`
+    return cssText = `height: ${widthHeight}%; width: ${widthHeight}%;`;
+}
+
+function addEventlistenerToPart(canvasPart, cssrule) {
+    canvasPart.addEventListener('mouseover', function() {
+        canvasPart.style.cssText=`background-color: ${chosenColor}; ${cssrule}`
+    })
 }
 
 createCanvasSegments()
-calculateCanvasPartSize()
