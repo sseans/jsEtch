6fa4670cc06c8d2e351dc94ada637e8c9ad3fb0b
sseans <41116820+sseans@users.noreply.github.com>
2021-02-01 09:26:43 -0700

    refactor of functions and logic to make reusability better

--- app.js
+++ app.js
@@ -1,34 +1,133 @@
 // Variables
-let canvasSize = 32;
+let canvasSize = 16;
 let chosenColor = 'yellow'
+let penActive = false;
+let canvasPart = [];
 
 // Selectors
 const canvas = document.querySelector('.canvas')
+const clearButton = document.querySelector('#clearbutton')
+
+// EventListeners
+clearButton.addEventListener('click', resetCanvas)
+canvas.addEventListener('click', togglePen)
+
+// Controls 
+// Slider Input
+sliderInput = document.querySelector('#sliderinput')
+sliderInput.addEventListener('change', function() {
+    console.log(sliderInput.value);
+    canvasSize = sliderInput.value;
+    createCanvasSegments(canvasSize)
+})
+
 
 // Functions
-function createCanvasSegments() {
-    for (let i = 0; i < canvasSize; i++) {
-        for (let i = 0; i < canvasSize; i++) {
-            const canvasPart = document.createElement('div')
-            canvasPart.className = 'canvas__part'
-            canvas.appendChild(canvasPart)
-
-            let cssrule = calculatePartSize(canvasPart, canvasSize)
-            addEventlistenerToPart(canvasPart, cssrule)
-        }
-    }  
+function createCanvasSegments(canvasSize) {
+    destroyCanvas()
+    canvas.style.gridTemplateColumns = `repeat(${canvasSize}, 1fr)`
+    canvas.style.gridTemplateRows = `repeat(${canvasSize}, 1fr)`
+    let numberOfParts = canvasSize * canvasSize
+    for (let i = 0; i < numberOfParts; i++) {
+        canvasPart[i] = document.createElement('div')
+        canvasPart[i].className = 'canvas__part'
+        canvas.appendChild(canvasPart[i])
+    }
 }
 
-function calculatePartSize(component, desiredSize) {
-    let widthHeight = 100 / desiredSize   
-    component.style.cssText=`height: ${widthHeight}%; width: ${widthHeight}%;`
-    return cssText = `height: ${widthHeight}%; width: ${widthHeight}%;`;
+function destroyCanvas() {
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.firstChild)
+    }
 }
 
-function addEventlistenerToPart(canvasPart, cssrule) {
-    canvasPart.addEventListener('mouseover', function() {
-        canvasPart.style.cssText=`background-color: ${chosenColor}; ${cssrule}`
-    })
+function resetCanvas() {
+    destroyCanvas()
+    createCanvasSegments(canvasSize)
 }
 
-createCanvasSegments()
+function togglePen() {
+    if (penActive == false) {
+        penActive = true; 
+        canvasPart.forEach(element => {
+            element.addEventListener('mouseover', paint())
+        });
+    } else {
+        penActive = false;
+        canvasPart.forEach(element => {
+            element.removeEventListener('mouseover', paint())
+        })
+    }
+}
+
+function paint() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     // loops through creating canvas elements based on desired canvas size
+//     for (let i = 0; i < canvasSize; i++) {
+//         for (let i = 0; i < canvasSize; i++) {
+//             // creates canvas elements & appends
+//             canvasPart = document.createElement('div')
+//             canvasPart.className = 'canvas__part'
+//             canvas.appendChild(canvasPart)
+//             // changes the size of the elements & adds the mouseover listener
+//             let cssWidthHeight = calculatePartSize(canvasPart, canvasSize)
+//             addEventlistenerToPart(canvasPart, cssWidthHeight)
+//         }
+//     }  
+// }
+
+// function calculatePartSize(component, desiredSize) {
+//     let widthHeight = 100 / desiredSize
+//     let cssSizeStyling = `height: ${widthHeight}%; width: ${widthHeight}%;`;  
+//     component.style.cssText = cssSizeStyling
+//     return cssSizeStyling
+// }
+
+// function addEventlistenerToPart(canvasPart, cssWidthHeight) {
+//     canvasPart.addEventListener('mouseover', function() {
+//         canvasPart.style.cssText=`background-color: ${chosenColor}; ${cssWidthHeight}`
+//     })
+    
+// }
+
+// function togglePen() {
+//     if (penActive == false) {
+//         addEventlistenerToPart()
+//     }
+    
+// }
+
+// createCanvasSegments()
