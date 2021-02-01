921097715e3c8d8c47f9eec05f95b5dcacdbe7c5
sseans <41116820+sseans@users.noreply.github.com>
2021-02-01 15:24:35 -0700

    added drawing functionality & minor styling

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Variables
-let canvasSize = 16;
+let canvasSize = 32;
 let chosenColor = 'yellow'
 let penActive = false;
 let canvasPart = [];
@@ -7,19 +7,25 @@ let canvasPart = [];
 // Selectors
 const canvas = document.querySelector('.canvas')
 const clearButton = document.querySelector('#clearbutton')
+const sliderDisplayNumber = document.querySelector('#sliderdisplay')
+const colorPicker = document.querySelector('#colorpicker')
 
 // EventListeners
 clearButton.addEventListener('click', resetCanvas)
 canvas.addEventListener('click', togglePen)
+window.onload = createCanvasSegments(canvasSize)
 
 // Controls 
-// Slider Input
-sliderInput = document.querySelector('#sliderinput')
-sliderInput.addEventListener('change', function() {
-    console.log(sliderInput.value);
-    canvasSize = sliderInput.value;
-    createCanvasSegments(canvasSize)
-})
+    // Slider Input
+    sliderInput = document.querySelector('#sliderinput')
+    sliderInput.addEventListener('change', function() {
+        console.log(sliderInput.value);
+        canvasSize = sliderInput.value;
+        createCanvasSegments(canvasSize)
+    })
+    sliderInput.addEventListener('input', function() {
+        sliderDisplayNumber.innerText = sliderInput.value
+    }) 
 
 
 // Functions
@@ -46,22 +52,23 @@ function resetCanvas() {
     createCanvasSegments(canvasSize)
 }
 
+// On/Off Pen Input (Loops through canvasPart array adding or removing eventlistener)
 function togglePen() {
     if (penActive == false) {
         penActive = true; 
         canvasPart.forEach(element => {
-            element.addEventListener('mouseover', paint())
+            element.addEventListener('mouseover', paint)
         });
     } else {
         penActive = false;
         canvasPart.forEach(element => {
-            element.removeEventListener('mouseover', paint())
+            element.removeEventListener('mouseover', paint)
         })
     }
 }
 
-function paint() {
-
+function paint(e) {
+    e.target.style.background = colorPicker.value    
 }
 
 
